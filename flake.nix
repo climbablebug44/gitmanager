0a525594d93d611efdf984453e1ccccedcf89657
Ashwin Nair <ashwinonline2000@gmail.com>
2024-07-14 23:04:59 +0530

    sync

--- flake.nix
+++ flake.nix
@@ -5,45 +5,70 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      archs = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "riscv64-linux"
+      ];
+      genStr = key: value: "${key}:${value}";
+    in
+    {
 
-    packages = genAttrs archs (system: with nixpkgs.legacyPackages.${system}; rec {
-      gitmanager = nixpkgs.stdenv.mkDerivation {
-        src = ./src;
-        unpack = false;
-        installPhase = ''
-          # $out is an automatically generated filepath by nix,
-          # but it's up to you to make it what you need. We'll create a directory at
-          # that filepath, then copy our sources into it.
-          mkdir $out/bin
-          cp -rv $src/* $out
-        '';
-
-      };
-
-      nixosModules.gitmanager = {
-        options.services.gitmanager = {
-          enable = mkEnableOption "Gitmanager Service";
+      packages = nixpkgs.lib.genAttrs archs (system: with nixpkgs.legacyPackages.${system}; rec {
+        gitmanager = nixpkgs.stdenv.mkDerivation {
+          src = ./src;
+          unpack = false;
+          installPhase = ''
+            # $out is an automatically generated filepath by nix,
+            # but it's up to you to make it what you need. We'll create a directory at
+            # that filepath, then copy our sources into it.
+            mkdir $out/bin
+            cp -rv $src/* $out
+          '';
         };
 
-        config = mkIf config.services.gitmanager.enable {
-          system.fsPackages = [ self.packages.${pkgs.system}.gitmanager ];
-          systemd = {
-            timers.gimanager = {
-              
+        nixosModules.gitmanager = {
+          options.services.gitmanager = {
+            enable = mkEnableOption "Gitmanager Service";
+            freq = mkOption {
+              type = with types; str;
+              default = "60m";
             };
-            services.gitmanager = {
-              wantedBy = [ "multi-user.target" ];
-              path = with pkgs; [
-                git
-                self.packages.${pkgs.system}.gitmanager
-              ];
-              script = "gitmanager";
+            repos = mkOption {
+              type = with types; listOf attrs;
+              default = [ ];
+            };
+          };
+
+          config = mkIf config.services.gitmanager.enable {
+            system.fsPackages = [ self.packages.${pkgs.system}.gitmanager ];
+            systemd = {
+              timers.gitmanager = {
+                wantedBy = [ "timers.target" ];
+                timerConfig = {
+                  OnBootSec = cfg.freq;
+                  OnUnitActiveSec = cfg.freq;
+                  Unit = "gitmanager.service";
+                };
+              };
+              services.gitmanager = {
+                wantedBy = [ "multi-user.target" ];
+                path = with pkgs; [
+                  git
+                  self.packages.${pkgs.system}.gitmanager
+                ];
+                serviceConfig = {
+                  Type = "oneshot";
+                  User = "root";
+                };
+                script = "gitmanager ${lib.concatStrings map genStr config.repos }";
+              };
             };
           };
         };
-
-      };
-    });
-  };
+      });
+    };
 }
+    
